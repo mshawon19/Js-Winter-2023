00a4886dc8c917fa838e8bacd24331f464b559d8
Mohammed Shawon <shawon@Mohammeds-MacBook-Pro.local>
2023-02-25 12:38:08 -0500

    Run Codes for Homework 9

--- Modules1/classProject/Institute.js
+++ Modules1/classProject/Institute.js
@@ -1,12 +1,18 @@
 const Student = require("./students");
+const Teacher = require("./teachers");
 
 
 const s1 = new Student();
 s1.enroll('John Doe', 16, 'manUaL QA');
-s1.changeName('Shawon ')
+s1.showMyDetails();
 
-const s2 = new Student();
-s2.enroll('Md Shawon', 23, 'manual QA')
-s2.changeName('Md Fahad Shawon');
-s2.changeCourse('SDET Course');
-s2.MakePayment(250);
+
+const t1 = new Teacher;
+t1.hire('John Doe', 27, 'Web Development')
+t1.showMyDetails();
+t1.changeName('Prof. Shawon');
+t1.gradeStudent(1, 'A')
+
+s1.showMyDetails();
+t1.updateGrade(1, 'C+')
+s1.showMyDetails();
